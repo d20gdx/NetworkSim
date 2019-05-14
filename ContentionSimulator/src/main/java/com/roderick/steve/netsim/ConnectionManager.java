bb870fe62a5cd5edbb30041d1b995683ea597a35
sroderick <sroderick@VM-104680-01.zellis.com>
2019-05-14 16:04:50 +0100

    code format

--- ContentionSimulator/src/main/java/com/roderick/steve/netsim/ConnectionManager.java
+++ ContentionSimulator/src/main/java/com/roderick/steve/netsim/ConnectionManager.java
@@ -6,22 +6,18 @@ package com.roderick.steve.netsim;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-
 
 /**
  * Class to manage simulated connections
  */
 public class ConnectionManager {
-    
+
     private static final Logger log4j = LogManager.getLogger(ConnectionManager.class.getName());
     private static Map<Integer, Integer> channelOccupancy = new HashMap<Integer, Integer>();
     private static SimParameters props;
 
-
-    
     /**
      * Set the properties for connection manager
      * 
@@ -30,7 +26,7 @@ public class ConnectionManager {
     public static void setProperties(SimParameters properties) {
         props = properties;
     }
-        
+
     /**
      * Instigate a new connection for a given channel and modulation rate
      * 
@@ -42,7 +38,7 @@ public class ConnectionManager {
     public static synchronized boolean makeConnection(int channel, int sf, int nodeId) {
 
         float percentage = 0f;
-        if (props.getTotalPacketsSent()!= 0) {
+        if (props.getTotalPacketsSent() != 0) {
             percentage = (float) (props.getPacketsLost() * 100 / props.getTotalPacketsSent());
         }
 
@@ -50,18 +46,19 @@ public class ConnectionManager {
         long duration = (System.currentTimeMillis() - props.getStartTime()) / 1000;
         if (duration > props.getRunDurationSecs()) {
             System.out.println("Completed run," + duration + "," + sf + "," + props.getNoNodesInRun());
-            log4j.trace("Completed run," + duration + "," + sf + "," + props.getNoNodesInRun()); 
+            log4j.trace("Completed run," + duration + "," + sf + "," + props.getNoNodesInRun());
             return false;
         }
 
-        // Calculate the percentage of packets loss. Assume network 
+        // Calculate the percentage of packets loss. Assume network
         // congestion at levels of 80% loss and above. Cease to send
         // further connection requests
         if (percentage > 80f) {
             if (!props.isNetworkSwamped()) {
-                log4j.trace("Network congestion detected," + duration + "," + sf + "," + props.getNoNodesInRun()); 
-                System.out.println("Network congestion detected," + duration + "," + sf + "," + props.getNoNodesInRun());
-                props.setNetworkSwamped(true); 
+                log4j.trace("Network congestion detected," + duration + "," + sf + "," + props.getNoNodesInRun());
+                System.out
+                        .println("Network congestion detected," + duration + "," + sf + "," + props.getNoNodesInRun());
+                props.setNetworkSwamped(true);
             }
             return false;
         }
@@ -72,7 +69,7 @@ public class ConnectionManager {
         }
 
         int count = (int) channelOccupancy.get(channel);
-        int totalPacketsSent = props.getTotalPacketsSent();        
+        int totalPacketsSent = props.getTotalPacketsSent();
         props.setTotalPacketsSent(totalPacketsSent++);
 
         // determine if all channels are currently occupied
@@ -84,7 +81,7 @@ public class ConnectionManager {
         } else {
             channelOccupancy.put(channel, count++);
             // start TimeOut Thread
-            (new Thread(new ConnectionThread(props.getTargetGateway(),channel, sf))).start();
+            (new Thread(new ConnectionThread(props.getTargetGateway(), channel, sf))).start();
         }
         return true;
     }
--- ContentionSimulator/src/main/java/com/roderick/steve/netsim/Runner.java
+++ ContentionSimulator/src/main/java/com/roderick/steve/netsim/Runner.java
@@ -3,21 +3,24 @@
  *  Date: 21/12/2018                                                    *
  ***********************************************************************/
 package com.roderick.steve.netsim;
-import org.apache.logging.log4j.Logger; 
+
+import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.LogManager;
+
 /**
  * Test harness and main class runner
  *
  */
 public class Runner {
     private static final Logger log4j = LogManager.getLogger(Runner.class.getName());
-    static final String DEFAULT_GATEWAY = "Multitech"; 
-    static final int START_SF = 7; 
-    static final int START_RUN_DURATION_SECS = 60; 
-    static final int INCREASE_RUN_STEP_SIZE = 30; 
+    static final String DEFAULT_GATEWAY = "Multitech";
+    static final int START_SF = 7;
+    static final int START_RUN_DURATION_SECS = 60;
+    static final int INCREASE_RUN_STEP_SIZE = 30;
+
     /**
-     * Main method to execute LPWAN Network Contention Simulator
-     * Run with gateway name parameter to switch targets
+     * Main method to execute LPWAN Network Contention Simulator Run with gateway
+     * name parameter to switch targets
      * 
      * @param args - Gateway type (Multitech / TTN)
      */
@@ -31,22 +34,24 @@ public class Runner {
         props.setRunDurationSecs(START_RUN_DURATION_SECS);
         props.setTargetGateway(args.length > 0 ? args[0] : DEFAULT_GATEWAY);
         props.setSpreadingFactor(START_SF);
-        
+
         // output text headers in console for CSV output
         System.out.println("Status, Duration, Spreading Factor, Number of Nodes");
 
-        for (int run = props.getMinNodesInRun(); run <= props.getMaxNodesInRun(); run += props.getIncreaseRunStepSize()) {
-           
+        for (int run = props.getMinNodesInRun(); run <= props.getMaxNodesInRun(); run += props
+                .getIncreaseRunStepSize()) {
+
             props.setNetworkSwamped(false);
             props.setStartTime(System.currentTimeMillis());
             props.setNoNodesInRun(run);
-    
-            System.out.println("Starting run gateway " + props.getTargetGateway() + " for " + run + " nodes at spreading factor " + props.getSpreadingFactor());
+
+            System.out.println("Starting run gateway " + props.getTargetGateway() + " for " + run
+                    + " nodes at spreading factor " + props.getSpreadingFactor());
 
             ConnectionManager.setProperties(props);
-            
+
             for (int i = 1; i <= run; i++) {
-                log4j.trace("spooling new thread"); 
+                log4j.trace("spooling new thread");
                 (new Thread(new NodeThread(i, props.getSpreadingFactor()))).start();
             }
 
@@ -57,7 +62,7 @@ public class Runner {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            
+
             // check that all nodes have completed
             while (NodeThread.numberOfNodes > 0) {
                 System.out.println("Number of nodes:" + NodeThread.numberOfNodes);
